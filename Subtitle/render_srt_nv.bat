aa19c0d995d9321a8d476e7d3e3334258588c279
serpentvar <varshark@msn.com>
2023-09-17 03:25:31 +0800

    update render method and add nv encode

--- Subtitle/render_srt_nv.bat
+++ Subtitle/render_srt_nv.bat
@@ -7,7 +7,7 @@ set "input_file=%~1"
 set "subtitle_file=!input_file:~0,-4!.srt"
 set "output_file=!input_file:~0,-4!_with_subtitles.mp4"
 
-"ffmpeg.exe" -i "!input_file!" -i "!subtitle_file!" -c:v libx264 -max_muxing_queue_size 9999 -preset veryslow -crf 18 -c:a copy -c:s mov_text -scodec mov_text "!output_file!"
+"ffmpeg.exe" -i "!input_file!" -i "!subtitle_file!" -c:v h264_nvenc -preset medium -b:v 16000k -c:a copy -c:s mov_text "!output_file!"
 shift
 goto :loop
 
